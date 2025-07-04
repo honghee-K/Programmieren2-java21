04cc7419e7429a7d3670c92cbfb674eecdf74d2c
Honghee <honghee.kim25@gmail.com>
2025-07-04 14:31:28 +0200

    Person

---
+++ src/main/java/Exercise/AK/WS22_23/A2/Person.java
@@ -0,0 +1,42 @@
+package Exercise.AK.WS22_23.A2;
+
+import java.io.*;
+
+public class Person implements Serializable {
+    public String name;
+    public int age;
+
+    public Person(String name, int alt){
+        this.name = name;
+        this.age = alt;
+    }
+
+    public Person gibKopie (Person p){
+        try(ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos)){
+                oos.writeObject(p);
+                oos.flush();
+
+                byte[] bytes = baos.toByteArray();
+
+            try(ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+                ObjectInputStream ois = new ObjectInputStream(bais)){
+
+                Person copiePerson = (Person) ois.readObject();
+                return copiePerson;
+
+            } catch(IOException | ClassNotFoundException e){
+                e.printStackTrace();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    @Override
+    public String toString(){
+        return "Person{" + "name: " + name + ", age: " + age + "}";
+    }
+
+}
---
+++ src/test/java/CopyPersonTest.java
@@ -0,0 +1,49 @@
+import Exercise.AK.WS22_23.A2.Person;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class CopyPersonTest {
+
+    @Test
+    public void testCopyPerson(){
+        Person p = new Person("Luis", 24);
+        System.out.println(p.gibKopie(p));
+
+
+    }
+
+    @Test
+    public void testCopyPersonWithStream() throws IOException, ClassNotFoundException {
+        Person p = new Person("Luis", 24);
+        Person copiedPerson = null;
+
+        try(ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);){
+            oos.writeObject(p);
+            oos.flush();
+
+            byte[] bytes = baos.toByteArray();
+
+            try(ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+                ObjectInputStream ois = new ObjectInputStream(bais)){
+
+                copiedPerson = (Person) ois.readObject();
+
+
+            }
+            assertEquals("Luis", copiedPerson.name);
+            assertEquals(24,copiedPerson.age);
+            assertNotNull(copiedPerson);
+
+        }
+
+
+
+
+    }
+}
