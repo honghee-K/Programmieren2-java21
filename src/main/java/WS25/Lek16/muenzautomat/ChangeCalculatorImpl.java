b9f7d4b1eca33b8f3e47b602fe865aec3e34e3b9
Honghee Kim <honghee.kim@study.thws.de>
2025-11-05 23:33:02 +0100

    Lek16 update

--- src/main/java/WS25/Lek16/muenzautomat/ChangeCalculatorImpl.java
+++ src/main/java/WS25/Lek16/muenzautomat/ChangeCalculatorImpl.java
@@ -1,16 +1,24 @@
 package WS25.Lek16.muenzautomat;
 
 public class ChangeCalculatorImpl implements ChangeCalculator{
+
+
+
     @Override
     public int[] getChange(int euros, int cent){
 
         Coin[] coins = {Coin.EURO_2, Coin.EURO_1, Coin.CENT_50, Coin.CENT_20, Coin.CENT_10, Coin.CENT_5, Coin.CENT_2, Coin.CENT_1};
-        int amount = 157;
-        for(int i = 0; i < coins.length; i++){
-            //Todo
+        int[] result = new int[coins.length];
+        int[] mapping = {7, 6, 5, 4, 3, 2, 1, 0};
+        int amount = euros * 100 + cent;
 
+        for(int i = 0; i < coins.length; i++){
+            int count = amount / coins[i].value;
+            amount = amount % coins[i].value;
+            result[mapping[i]] = count;
         }
 
-        return null;
+        return result;
     }
+
 }
